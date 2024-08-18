127b0a0d07a042cd191299f191506d52a879a200
Komilov Ruslonbek <kamilovruslan2022@gmail.com>
2024-08-18 12:31:09 +0500

    exam

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { TokenContext } from './context/Context.jsx'
 
 
 createRoot(document.getElementById('root')).render(
